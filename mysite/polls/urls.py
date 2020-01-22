7e01cafa3bf2702374b0860b1448cf81f265700d
HanByul Yang <hanbyul.yang@gmail.com>
2020-01-22 16:59:21 +0900

    Writing your first Django app, part 1
        - https://docs.djangoproject.com/en/3.0/intro/tutorial01/

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('', views.index, name='index'),
 ]
